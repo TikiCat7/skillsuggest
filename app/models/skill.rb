e95ea29cb2811c56e2659d6463f96f3ec81b8670
Wataru Kay <wata1991@gmail.com>
2016-08-14 22:19:34 +0900

    Add more validations for skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,4 +2,7 @@ class Skill < ApplicationRecord
   belongs_to :Profile, optional: true
   validates :skillName, uniqueness: { scope: [:assignee,:Profile_id],
     message: "Can only assign one skill per person once" }
+  validates :skillName,  presence: true, length: { maximum: 50 }
+  validates :assignee,  presence: true
+  validates :Profile_id, presence: true
 end
