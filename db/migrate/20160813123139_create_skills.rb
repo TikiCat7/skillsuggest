761589558edcf18fe5a54c753dea74357a3331de
Wataru Kay <wata1991@gmail.com>
2016-08-14 19:03:09 +0900

    change schema for profiles

--- db/migrate/20160813123139_create_skills.rb
+++ db/migrate/20160813123139_create_skills.rb
@@ -2,7 +2,7 @@ class CreateSkills < ActiveRecord::Migration[5.0]
   def change
     create_table :skills do |t|
       t.string :skillName
-      t.references :Profile, foreign_key: true
+      t.references :profile, foreign_key: true
       t.string :assignee
 
       t.timestamps
